bd0d3f1e10d443cf5f1fd0c3d9650d00e02ff319
Menna114 <mennadiaah14@gmail.com>
2025-02-28 20:25:19 +0200

    creating backend bucket for my states and private, public subnet for my vbc

---
+++ subnets.tf
@@ -0,0 +1,18 @@
+resource "aws_subnet" "private_sub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "private_sub"
+  }
+}
+resource "aws_subnet" "public_sub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "public_sub"
+  }
+}
