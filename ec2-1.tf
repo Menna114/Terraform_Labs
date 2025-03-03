75d13091624f48057a3fe3cef227f927d49e2850
Menna114 <mennadiaah14@gmail.com>
2025-03-03 06:21:39 +0200

    created an output file to test the public ip of bastion host

--- ec2-1.tf
+++ ec2-1.tf
@@ -3,8 +3,16 @@ resource "aws_instance" "bastion" {
   instance_type = var.instance_type
   subnet_id = aws_subnet.public_sub.id
 
+  associate_public_ip_address = true
+  
   tags = {
     Name = "terrafrom-bastion"
   }
   vpc_security_group_ids =[aws_security_group.secruity-group-1.id]
+  
+  provisioner "local-exec"{
+    command= "echo ${self.public_ip} > inv.txt"
+  }
+
 }
+
